0ffeeab1d8c05dca12bbb3839a673636b6afd6a9
THanae <40744042+THanae@users.noreply.github.com>
2025-06-23 14:11:37 +0000

    Deploying to gh-pages from  @ 274a41cff9a72d8bd90600bf806f054e0a266293 🚀

---
+++ classShipMuonShield.js
@@ -0,0 +1,24 @@
+var classShipMuonShield =
+[
+    [ "ShipMuonShield", "classShipMuonShield.html#a5d5ad91df9fc8ad44b3d77c9f4fdcd14", null ],
+    [ "ShipMuonShield", "classShipMuonShield.html#a21c4058626af3ba4afb7100f8d1e4023", null ],
+    [ "~ShipMuonShield", "classShipMuonShield.html#a14c09abf8f8f662a955ecd22a02dae21", null ],
+    [ "ConstructGeometry", "classShipMuonShield.html#a3ed6f246da768e38f2fe775dd523c0be", null ],
+    [ "CreateArb8", "classShipMuonShield.html#ac3d03b895b44fbf2164d5202fd661287", null ],
+    [ "CreateMagnet", "classShipMuonShield.html#adc4671e1413b00224c312f8f55ec65b7", null ],
+    [ "Initialize", "classShipMuonShield.html#a581dc74485cbcb3e8e5ee0871efe1c7a", null ],
+    [ "InitMedium", "classShipMuonShield.html#ac4224450310d00f431be3d75bb3d6904", null ],
+    [ "dXgap", "classShipMuonShield.html#a6e723d2d444d09e01c9c78e603bb9403", null ],
+    [ "dZ0", "classShipMuonShield.html#ab069ded75693602ee71e1bc234b7e7d3", null ],
+    [ "dZ1", "classShipMuonShield.html#ae4e32b0424587f39cfebe17a61802b4a", null ],
+    [ "dZ2", "classShipMuonShield.html#a389e2d50fdef145de5619b65f6e58d43", null ],
+    [ "dZ3", "classShipMuonShield.html#ab511b514fc5c3c8b2c0c17af12f68deb", null ],
+    [ "dZ4", "classShipMuonShield.html#af436f2c092254025e50da09168a52ab7", null ],
+    [ "dZ5", "classShipMuonShield.html#a383ed5b7180b523f436973026401b725", null ],
+    [ "dZ6", "classShipMuonShield.html#a670e1ef878312900ca31d8b81d8eea20", null ],
+    [ "dZ7", "classShipMuonShield.html#aebcb6ee4cbda95d16913555619d7e5bf", null ],
+    [ "fSC_mag", "classShipMuonShield.html#a92c765f3f0e52c87e24d34520480868d", null ],
+    [ "fWithConstShieldField", "classShipMuonShield.html#aa83bbeaa79fad431c2c5186c7df276ba", null ],
+    [ "shield_params", "classShipMuonShield.html#aa20eee4086760aa148d9aca889ad0746", null ],
+    [ "zEndOfProxShield", "classShipMuonShield.html#ae18f1c86db999626d60d03eb351f071e", null ]
+];
---
+++ navtreedata.js
@@ -0,0 +1,153 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "FairShip", "index.html", [
+    [ "FairShip documentation", "index.html", null ],
+    [ "Changelog", "md_CHANGELOG.html", [
+      [ "Unreleased", "md_CHANGELOG.html#autotoc_md1", [
+        [ "Added", "md_CHANGELOG.html#autotoc_md2", null ],
+        [ "Fixed", "md_CHANGELOG.html#autotoc_md3", null ],
+        [ "Changed", "md_CHANGELOG.html#autotoc_md4", null ],
+        [ "Removed", "md_CHANGELOG.html#autotoc_md5", null ]
+      ] ],
+      [ "25.01", "md_CHANGELOG.html#autotoc_md6", [
+        [ "Added", "md_CHANGELOG.html#autotoc_md7", null ],
+        [ "Fixed", "md_CHANGELOG.html#autotoc_md8", null ],
+        [ "Changed", "md_CHANGELOG.html#autotoc_md9", null ],
+        [ "Removed", "md_CHANGELOG.html#autotoc_md10", null ]
+      ] ],
+      [ "24.11", "md_CHANGELOG.html#autotoc_md11", [
+        [ "Added", "md_CHANGELOG.html#autotoc_md12", null ],
+        [ "Fixed", "md_CHANGELOG.html#autotoc_md13", null ],
+        [ "Changed", "md_CHANGELOG.html#autotoc_md14", null ],
+        [ "Removed", "md_CHANGELOG.html#autotoc_md15", null ]
+      ] ],
+      [ "24.10 - Freiburg", "md_CHANGELOG.html#autotoc_md16", [
+        [ "Added", "md_CHANGELOG.html#autotoc_md17", null ],
+        [ "Fixed", "md_CHANGELOG.html#autotoc_md18", null ],
+        [ "Changed", "md_CHANGELOG.html#autotoc_md19", null ],
+        [ "Removed", "md_CHANGELOG.html#autotoc_md20", null ]
+      ] ]
+    ] ],
+    [ "README", "md_field_2README.html", null ],
+    [ "FairShip", "md_README.html", [
+      [ "Introduction", "md_README.html#autotoc_md23", [
+        [ "Branches", "md_README.html#autotoc_md24", null ]
+      ] ],
+      [ "Build Instructions using CVMFS", "md_README.html#autotoc_md25", null ],
+      [ "Local build, without access to CVMFS", "md_README.html#autotoc_md26", null ],
+      [ "Run instructions", "md_README.html#autotoc_md27", null ],
+      [ "Docker instructions", "md_README.html#autotoc_md28", null ],
+      [ "Troubleshooting information", "md_README.html#autotoc_md29", null ],
+      [ "Documentation", "md_README.html#autotoc_md30", null ],
+      [ "Contributing code", "md_README.html#autotoc_md31", null ]
+    ] ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "namespacemembers_vars.html", "namespacemembers_vars" ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"AddDiMuonDecayChannelsToG4_8py.html",
+"ShipGeoCave_8cxx_source.html",
+"TTCluster_8py_source.html",
+"classDPPythia8Generator.html#a0ec98e4205363314266e3b2df49db2fe",
+"classGenieGenerator.html#a29396a1cf89c0f75beefde980874eefc",
+"classMagneticSpectrometer.html#a418bd7f6efacf510e2eccd17a08bfb42",
+"classNuTauMudet.html#aa9c48b16739e6bdf2582b826d3068d82",
+"classShipBFieldMap.html#a40d2bc90d23707faac34eba21b77c5dd",
+"classShipFieldPar.html#af0fadc8310f0f1409b3fb50731b10420",
+"classShipStack.html#a8e332a32b9ea1c6f27dc3b7a3ee55567",
+"classTargetTracker.html#a5e2f51fa3a7a9ca28903f1231e17597e",
+"classUpstreamTaggerPoint.html#abe4508d7a6d61fb21cab5603e0247bd9",
+"classecalCluster.html",
+"classecalLightMap.html#a742f984716b6f92d02939f652d27f0d6",
+"classeventDisplay_1_1DrawTracks.html",
+"classg4Ex__args_1_1MyGeneratorAction.html#a5aaf55335da702726de05a72186ae0ec",
+"classhcalInf.html#ad971dde8cd1cf69d5eaad1eb4a6cff3f",
+"classmethod__logger_1_1MethodLogger.html#a2c1dd9294b1059555813cb06da8b94cd",
+"classrpvsusy_1_1constants.html#afcd1e1d6d7b34070d793f16f1b52d08d",
+"classshipPid_1_1Task.html#a77f3ea64bd9649c22ddf2c6cd703aace",
+"classsimpleTarget.html#aea4b317e38925b10544fdd76792e8f26",
+"classstrawtubes.html#aafc2fe3022ca5ada943bcbb9b1f0eef8",
+"classveto.html#af8a387bf000115714b4b1e4711da7d9c",
+"ecalInf_8cxx.html",
+"functions_func_~.html",
+"geometry__config_8py.html#a16245ef0cfdc07cc742ece55062070e0",
+"hcalModuleMC_8cxx_source.html",
+"makeCascade_8py.html#ab856c202f2c9627c35c58fd90634855d",
+"md_README.html#autotoc_md30",
+"namespaceShipReco.html#a6425d90b37c7ae2428f3b732fc2d03a4",
+"namespacecheckZpositions.html#a0070d4bd74e3b9a7ed335c4abcff51ef",
+"namespaceextractNeutrinosAndUpdateWeight.html#ada8cdca68067f6a53d1115bf882b2d2c",
+"namespacegeometry__config.html#a497974e47a86565ede97bd6b328a85bc",
+"namespacehepunit.html#a8f02567280f2d170af1183e6b43e133a",
+"namespacemakeMuonDIS.html#aad6074c92021b815fcb6991e9e346baa",
+"namespacemergeMbias.html#a5544b0ec72dcfebd3ced70590287da18",
+"namespacerun__fixedTarget.html#a526b7d00dfe537eca8127724eac0e35c",
+"namespaceship.html#a83472731771521d366d09a96db58d50f",
+"namespacestudy__GammaConv.html#ade4e48b9a86cfadba1da8efe7fe47452",
+"rootpyPickler_8py.html#a540a57e3b678a887f6ee85c042c3798a",
+"run__simPgun_8py.html#ac99fcc9e29a39b3579ad293eda56e56d",
+"shipunit_8py.html#a598a1d502d83a7dd27edf8a96728124d",
+"study__muMSC_8py.html#a5d79158ac5e9fac73e754f02ddd23c14"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
